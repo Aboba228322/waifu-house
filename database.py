cba9ee8f831f88e39d814e172f3c567217a17fbe
Xtrip111 <44380460+Xtrip111@users.noreply.github.com>
2024-05-21 00:44:55 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-# database.py
-
 from sqlalchemy import create_engine, Column, Integer, BigInteger, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
