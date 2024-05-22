5d346494994afb8d6dcd4e598c3bb81cc471f6ad
Xtrip111 <Xtrip111@gmail.com>
2024-05-23 03:44:10 +0500

    Правка ссылки на сайт кассы.

--- handlers/payment.py
+++ handlers/payment.py
@@ -12,11 +12,8 @@ def get_payment_url(user_id, amount, requests_count):
     sign_data = f"{config.MERCHANT_ID}:{amount}:{config.SECRET_WORD_1}:RUB:{order_id}"
     sign = hashlib.md5(sign_data.encode()).hexdigest()
 
-
-    print(f"Sign Data: {sign_data}")
-
-    return (f"https://pay.freekassa.ru/?m={config.MERCHANT_ID}&oa={amount}&currency=RUB&o={order_id}&s={sign}"
-            f"&us_user_id={user_id}&us_requests_count={requests_count}")
+    return (f"https://pay.freekassa.ru/?m={config.MERCHANT_ID}&oa={amount}¤cy=RUB&o={order_id}&s={sign}"
+            f"&us_user_id={user_id}&us_requests_count={requests_count}"), sign_data
 
 async def payment_handler(message: types.Message):
     telegram_id = message.from_user.id
@@ -30,12 +27,12 @@ async def payment_handler(message: types.Message):
         session.add(user)
         session.commit()
 
-    payment_url = get_payment_url(user.id, amount, requests_count)
+    payment_url, sign_data = get_payment_url(user.id, amount, requests_count)
     keyboard_markup = InlineKeyboardMarkup(
         inline_keyboard=[[InlineKeyboardButton("Оплатить", url=payment_url)]]
     )
 
-    await message.reply("Для оплаты нажмите на кнопку ниже:", reply_markup=keyboard_markup)
+    await message.reply(f"Для оплаты нажмите на кнопку ниже:\n\nSign Data: {sign_data}", reply_markup=keyboard_markup)
 
 def register_handlers_payment(dp: Dispatcher):
     dp.register_message_handler(payment_handler, Text(equals="💲Оплатить подписку", ignore_case=True))
